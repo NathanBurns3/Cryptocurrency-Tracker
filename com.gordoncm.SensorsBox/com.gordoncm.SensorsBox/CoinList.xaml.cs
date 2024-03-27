1125846c7e63e9df3b2903b4030d76ed23c73e37
Cameron <cameron@cameron.com>
2024-03-27 19:34:05 -0400

    added load more btn and removed acitonbar next button

--- com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/CoinList.xaml.cs
+++ com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/CoinList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using com.gordoncm.SensorsBox.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace com.gordoncm.SensorsBox
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoinList : ContentPage
     {
+        CoinViewModel coinViewModel;
         public CoinList()
-        {
+        { 
             InitializeComponent();
+            BindingContext = coinViewModel = new CoinViewModel(); 
         }
     }
 }
