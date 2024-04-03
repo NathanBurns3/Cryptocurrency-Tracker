861026f8f0f3b0068078f0bad1678105f6cdbd0f
Cameron <cameron@cameron.com>
2024-04-02 20:17:00 -0400

    fixed typo

--- com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/Models/User.cs
+++ com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/Models/User.cs
@@ -12,6 +12,6 @@ namespace com.gordoncm.SensorsBox.Models
 
         public string Name { get; set; }
 
-        public string WalletAdress { get; set; }
+        public string WalletAddress { get; set; }
     }
 }
