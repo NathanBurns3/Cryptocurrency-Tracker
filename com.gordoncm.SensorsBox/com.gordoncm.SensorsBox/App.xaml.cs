25e9bd86151af3249bbb77bbbd870b9678e5e8d2
Nathan Burns <nate10nate@gmail.com>
2024-04-14 21:21:14 -0400

    Allow users to customize and save their preferred color theme and font size settings

--- com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/App.xaml.cs
+++ com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using com.gordoncm.SensorsBox.Database;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace com.gordoncm.SensorsBox
 {
@@ -14,8 +14,27 @@ namespace com.gordoncm.SensorsBox
             MainPage = new AppShell();
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+
+            // Fetch user settings from the database
+            CryptoDB database = await CryptoDB.Instance;
+            string primaryColorName = await database.GetUserPrimaryColorAsync();
+            string secondaryColorName = await database.GetUserSecondaryColorAsync();
+            string fontSizeName = await database.GetUserFontSizeAsync();
+
+            Color primaryColor = GetColorFromName(primaryColorName);
+            Color secondaryColor = GetColorFromName(secondaryColorName);
+            double fontSize = GetFontSizeFromName(fontSizeName);
+
+            // Update the global styles
+            ((Style)Resources["DynamicButtonStyle"]).Setters.Add(new Setter { Property = Button.BackgroundColorProperty, Value = secondaryColor });
+            ((Style)Resources["DynamicLabelStyle"]).Setters.Add(new Setter { Property = Label.TextColorProperty, Value = primaryColor });
+            ((Style)Resources["DynamicLabelStyle"]).Setters.Add(new Setter { Property = Label.FontSizeProperty, Value = fontSize });
+            ((Style)Resources["DynamicButtonStyle"]).Setters.Add(new Setter { Property = Button.FontSizeProperty, Value = fontSize });
+            ((Style)Resources["DynamicPickerStyle"]).Setters.Add(new Setter { Property = Picker.FontSizeProperty, Value = fontSize });
+            ((Style)Resources["DynamicEntryStyle"]).Setters.Add(new Setter { Property = Entry.FontSizeProperty, Value = fontSize });
+
         }
 
         protected override void OnSleep()
@@ -24,6 +43,50 @@ namespace com.gordoncm.SensorsBox
 
         protected override void OnResume()
         {
+        }
+
+        private Color GetColorFromName(string colorName)
+        {
+            switch (colorName)
+            {
+                case "Red":
+                    return Color.Red;
+                case "Blue":
+                    return Color.Blue;
+                case "Green":
+                    return Color.Green;
+                case "Yellow":
+                    return Color.Yellow;
+                case "Purple":
+                    return Color.Purple;
+                case "Pink":
+                    return Color.Pink;
+                case "Orange":
+                    return Color.Orange;
+                case "Brown":
+                    return Color.Brown;
+                case "Black":
+                    return Color.Black;
+                case "White":
+                    return Color.White;
+                default:
+                    return Color.Default;
+            }
+        }
+
+        private double GetFontSizeFromName(string fontSize)
+        {
+            switch (fontSize)
+            {
+                case "Small":
+                    return 12.0;
+                case "Medium":
+                    return 20.0;
+                case "Large":
+                    return 28.0;
+                default:
+                    return 16.0;
+            }
         }
     }
 }
--- com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/Settings.xaml.cs
+++ com.gordoncm.SensorsBox/com.gordoncm.SensorsBox/Settings.xaml.cs
@@ -32,6 +32,86 @@ namespace com.gordoncm.SensorsBox
             CryptoDB database = await CryptoDB.Instance;
             await database.UpdateUserAsync(vm.User);
             await Navigation.PopAsync();
+
+            string primaryColorName = await database.GetUserPrimaryColorAsync();
+            string secondaryColorName = await database.GetUserSecondaryColorAsync();
+            string fontSizeName = await database.GetUserFontSizeAsync();
+
+            Color primaryColor = GetColorFromName(primaryColorName);
+            Color secondaryColor = GetColorFromName(secondaryColorName);
+            double fontSize = GetFontSizeFromName(fontSizeName);
+
+            WalletAddressLabel.TextColor = primaryColor;
+            UsernameLabel.TextColor = primaryColor;
+            PreferredNameLabel.TextColor = primaryColor;
+            ColorPickerLabel.TextColor = primaryColor;
+            FontSizeLabel.TextColor = primaryColor;
+            CurrencyLabel.TextColor = primaryColor;
+
+            WalletAddressLabel.FontSize = fontSize;
+            UsernameLabel.FontSize = fontSize;
+            PreferredNameLabel.FontSize = fontSize;
+            ColorPickerLabel.FontSize = fontSize;
+            FontSizeLabel.FontSize = fontSize;
+            CurrencyLabel.FontSize = fontSize;
+
+            WalletAddressEntry.FontSize = fontSize;
+            UsernameEntry.FontSize = fontSize;
+            PreferredNameEntry.FontSize = fontSize;
+
+            PrimaryColorPicker.FontSize = fontSize;
+            SecondaryColorPicker.FontSize = fontSize;
+            FontSizePicker.FontSize = fontSize;
+            CurrencyPicker.FontSize = fontSize;
+
+            SaveButton.BackgroundColor = secondaryColor;
+            SaveButton.FontSize = fontSize;
+
+            OnAppearing();
+        }
+
+        private Color GetColorFromName(string colorName)
+        {
+            switch (colorName)
+            {
+                case "Red":
+                    return Color.Red;
+                case "Blue":
+                    return Color.Blue;
+                case "Green":
+                    return Color.Green;
+                case "Yellow":
+                    return Color.Yellow;
+                case "Purple":
+                    return Color.Purple;
+                case "Pink":
+                    return Color.Pink;
+                case "Orange":
+                    return Color.Orange;
+                case "Brown":
+                    return Color.Brown;
+                case "Black":
+                    return Color.Black;
+                case "White":
+                    return Color.White;
+                default:
+                    return Color.Default;
+            }
+        }
+
+        private double GetFontSizeFromName(string fontSize)
+        {
+            switch (fontSize)
+            {
+                case "Small":
+                    return 12.0;
+                case "Medium":
+                    return 20.0;
+                case "Large":
+                    return 28.0;
+                default:
+                    return 16.0;
+            }
         }
     }
 }
